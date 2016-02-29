b4b20507d2915dcb603abf38afef1ad30b254659
tmanthegamer <tmanthegamer@gmail.com>
2016-02-29 15:12:44 -0800

    Updated the main file to fix the segmentation fault issues. Had to malloc every Mesg struct.

--- Tests/main.cpp
+++ Tests/main.cpp
@@ -22,9 +22,10 @@ typedef struct
     char mesg_data[MAXMESSAGEDATA]; /* Small char array contains */
 } Mesg;
 
+
 int ReadMessage(int queue, Mesg* msg, long msg_type)
 {
-    int rc = msgrcv(queue, msg, MAXMSGSIZE, msg_type,0);
+    int rc = msgrcv(queue, msg, sizeof(Mesg), msg_type,0);
     if(rc < 0)
     {
         perror("msgrcv failed:\n");
@@ -58,7 +59,7 @@ int SendMessage(int queue, Mesg* msg)
 
     /* This will keep trying to send messages the message queue if there are
         too many messages in the queue. */
-    int rc = msgsnd(queue, msg, MAXMSGSIZE, 0);
+    int rc = msgsnd(queue, msg, sizeof(Mesg), 0);
 
     if (rc < 0)
     {
@@ -72,10 +73,13 @@ int SendMessage(int queue, Mesg* msg)
 int AddClientToList(int msgQueue, int client)
 {
     //need to read every message in the queue and fix it eventually.
-    Mesg oldMsg, newMsg;
+    Mesg *oldMsg, *newMsg;
+
+    oldMsg = (Mesg*)malloc(sizeof(Mesg));
+    newMsg = (Mesg*)malloc(sizeof(Mesg));
 
     //printf("Reading message");
-    if(ReadMessage(msgQueue, &oldMsg, 0) < 0)
+    if(ReadMessage(msgQueue, oldMsg, 0) < 0)
     {
         //printf("AddClientToList: No more messages.\n");
         return -1;
@@ -84,7 +88,7 @@ int AddClientToList(int msgQueue, int client)
     //printf("check message][");
 
     //msg here, modify it to add in another client
-    if(oldMsg.num_sockets <= 0)
+    if(oldMsg->num_sockets <= 0)
     {
         printf("AddClientToList: num_sockets lower than zero...\n");
         return -1;
@@ -92,33 +96,37 @@ int AddClientToList(int msgQueue, int client)
 
     //printf("check message2\n");
     //Do not allow too many connections
-    if(oldMsg.num_sockets >= MAXCONNECTIONS)
+    if(oldMsg->num_sockets >= MAXCONNECTIONS)
     {
-        printf("AddClientToList: num{%d} > %d...\n", oldMsg.num_sockets, MAXCONNECTIONS);
+        printf("AddClientToList: num{%d} > %d...\n", oldMsg->num_sockets, MAXCONNECTIONS);
         return -1;
     }
     //printf("adding new client\n");
 
     //Add in the new client and increase the number of sockets.
-    for(int i = 0; i < oldMsg.num_sockets; i++)
+    for(int i = 0; i < oldMsg->num_sockets; i++)
     {
         //printf("{%d}", i);
-        newMsg.sockets[i] = oldMsg.sockets[i];
+        newMsg->sockets[i] = oldMsg->sockets[i];
     }
 
-    newMsg.sockets[oldMsg.num_sockets] = client;
-    newMsg.num_sockets = oldMsg.num_sockets + 1;
-    newMsg.mesg_type = 1;
+    newMsg->sockets[oldMsg->num_sockets] = client;
+    newMsg->num_sockets = oldMsg->num_sockets + 1;
+    newMsg->mesg_type = 1;
+    memcpy(newMsg->mesg_data, oldMsg->mesg_data, sizeof(oldMsg->mesg_data));
 
     //printf("sending message");
 
     //Replace the old message
-    if(SendMessage(msgQueue, &newMsg) < 0)
+    if(SendMessage(msgQueue, newMsg) < 0)
     {
         printf("AddClientToList: sendmessage failure.\n");
         return -1;
     }
     //printf("Donerino message");
+
+    free(oldMsg);
+    free(newMsg);
 
     return 0;
 }
@@ -166,20 +174,24 @@ int main(void)
     }
 
 
-    Mesg msg, rcv;
+    Mesg *msg, *rcv;
     int i = 0;
 
-    msg.mesg_type = 1;
+    msg = (Mesg*)malloc(sizeof(Mesg));
+    rcv = (Mesg*)malloc(sizeof(Mesg));
+
+    msg->mesg_type = 1;
     for(i = 0; i < 200; i++)
     {
-        msg.sockets[i] = i*i;
-        msg.num_sockets = i + 1;
+        msg->sockets[i] = i+i;
+        msg->num_sockets = i + 1;
     }
-    printf("num:%d\n", msg.num_sockets);
-    sprintf(msg.mesg_data, "%s", "Hello.");
+    sprintf(msg->mesg_data, "%s", "Hello.");
+    printf("[num:%d][message:%s]\n", msg->num_sockets, msg->mesg_data);
+
 
     //int ReadMessage(int queue, Mesg* msg, long msg_type)
-    if(SendMessage(msgQueue, &msg) < 0)
+    if(SendMessage(msgQueue, msg) < 0)
     {
         printf("Sendmessage failure\n");
         RemoveQueue(msgQueue);
@@ -189,7 +201,7 @@ int main(void)
     printf("Sent message to queue.\n");
 
     //test stuff in this if statement below
-    int newClient = 12345;
+    int newClient = 123;
     if(AddClientToList(msgQueue, newClient) < 0)
     {
         printf("\nUnable to add a new client.\n");
@@ -199,18 +211,33 @@ int main(void)
 
     printf("Added client to list.\n");
 
-    if(ReadMessage(msgQueue, &rcv, 0) < 0)
+    Mesg *temp = (Mesg*)malloc(sizeof(Mesg));
+
+    if(ReadMessageCopy(msgQueue, temp, 0))
+    {
+        printf("Copy failure.\n");
+    }
+
+    printf("Info about the copied message...\n");
+    printf("[type:%ld][num_sockets:%d][message:%s]\n", temp->mesg_type, temp->num_sockets, temp->mesg_data);
+
+    free(temp);
+
+
+    if(ReadMessage(msgQueue, rcv, 0) < 0)
     {
         printf("Readmessage failure\n");
     }
 
     printf("Info about the message...\n");
-    printf("[type:%ld][num_sockets:%d][message:%s]\n", rcv.mesg_type, rcv.num_sockets, rcv.mesg_data);
+    printf("[type:%ld][num_sockets:%d][message:%s]\n", rcv->mesg_type, rcv->num_sockets, rcv->mesg_data);
 
-    for(i = 0; i < rcv.num_sockets; i++)
+    for(i = 0; i < rcv->num_sockets; i++)
     {
-        printf("<index:%d><socket:%d>\n", i, rcv.sockets[i]);
+        printf("<index:%d><socket:%d>\n", i, rcv->sockets[i]);
     }
+    free(msg);
+    free(rcv);
 
     RemoveQueue(msgQueue);
 
