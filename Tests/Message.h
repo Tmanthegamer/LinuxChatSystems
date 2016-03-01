e5d340f8f33448b1a2350088cb39b5036f45359a
tmanthegamer <tmanthegamer@gmail.com>
2016-02-29 19:11:16 -0800

    Redesigned the message queue files into a proper format. Also made main.cpp to be the main testing grounds for function tests.

---
+++ Tests/Message.h
@@ -0,0 +1,21 @@
+//
+// Created by tyler on 29/02/16.
+//
+
+#ifndef MESSAGEQUEUE_MESSAGE_H
+#define MESSAGEQUEUE_MESSAGE_H
+
+#define MAXMESSAGEDATA 	10
+#define MAXCONNECTIONS 	1500
+#define MAXMSGSIZE 		8192
+#define MSG_COPY		040000
+
+typedef struct
+{
+    long mesg_type; /* message type */
+    int sockets[MAXCONNECTIONS]; /* Array of socket connections */
+    int num_sockets; /* Number of sockets */
+    char mesg_data[MAXMESSAGEDATA]; /* Small char array contains */
+} Mesg;
+
+#endif //MESSAGEQUEUE_MESSAGE_H
--- Tests/main.cpp
+++ Tests/main.cpp
@@ -1,238 +1,10 @@
-#include <string.h>
-#include <stdio.h>
-#include <stdlib.h>
-#include <unistd.h>
-#include <mqueue.h>
-#include <fcntl.h>
-#include <signal.h>
-#include <sys/types.h>
-#include <sys/ipc.h>
-#include <sys/msg.h>
-
-#define MAXMESSAGEDATA 	10
-#define MAXCONNECTIONS 	1500
-#define MAXMSGSIZE 		8192
-#define MSG_COPY		040000
-
-typedef struct
-{
-    long mesg_type; /* message type */
-    int sockets[MAXCONNECTIONS]; /* Array of socket connections */
-    int num_sockets; /* Number of sockets */
-    char mesg_data[MAXMESSAGEDATA]; /* Small char array contains */
-} Mesg;
-
-int ReadMessage(int queue, Mesg* msg, long msg_type);
-int ReadMessageCopy(int queue, Mesg* msg, long msg_type);
-int RemoveClientFromList(int msgQueue, int client);
-int SendMessage(int queue, Mesg* msg);
-int AddClientToList(int msgQueue, int client);
-int SendFinalMessage(int queue, Mesg* msg);
-int RemoveQueue(int queue);
-int OpenQueue(void);
-int main(void);
-
-
-int ReadMessage(int queue, Mesg* msg, long msg_type)
-{
-    int rc = msgrcv(queue, msg, sizeof(Mesg), msg_type,0);
-    if(rc < 0)
-    {
-        perror("msgrcv failed:\n");
-        return -1;
-    }
-
-    return 0;
-}
-
-int ReadMessageCopy(int queue, Mesg* msg, long msg_type)
-{
-    //int flags = MSG_COPY | IPC_NOWAIT;
-    int rc = msgrcv(queue, msg, MAXMSGSIZE, msg_type, MSG_COPY | IPC_NOWAIT);
-    if(rc < 0)
-    {
-        perror("instant return\n");
-        return -1;
-    }
-
-    return 0;
-}
-
-int RemoveClientFromList(int msgQueue, int client)
-{
-    //need to read every message in the queue and fix it eventually.
-    Mesg *oldMsg, *newMsg;
-
-    oldMsg = (Mesg*)malloc(sizeof(Mesg));
-    newMsg = (Mesg*)malloc(sizeof(Mesg));
-
-    //printf("Reading message");
-    if(ReadMessage(msgQueue, oldMsg, 0) < 0)
-    {
-        //printf("RemoveClientFromList: No more messages.\n");
-        return -1;
-    }
-    //printf("[num:%d]\n", oldMsg.num_sockets);
-    //printf("check message][");
-
-    //msg here, modify it to add in another client
-    if(oldMsg->num_sockets <= 0)
-    {
-        printf("RemoveClientFromList: num_sockets lower than zero...\n");
-        return -1;
-    }
-
-    //printf("check message2\n");
-    //Do not allow too many connections
-    if(oldMsg->num_sockets >= MAXCONNECTIONS)
-    {
-        printf("RemoveClientFromList: num{%d} > %d...\n", oldMsg->num_sockets, MAXCONNECTIONS);
-        return -1;
-    }
-    //printf("removing the client\n");
-
-    //Add in the new client and increase the number of sockets.
-    for(int i = 0; i < oldMsg->num_sockets; i++)
-    {
-        //printf("{%d}", i);
-        if(oldMsg->sockets[i] != client)
-        {
-            newMsg->sockets[i] = oldMsg->sockets[i];
-        }
-    }
-
-    newMsg->num_sockets = oldMsg->num_sockets - 1;
-    newMsg->mesg_type = 1;
-    memcpy(newMsg->mesg_data, oldMsg->mesg_data, sizeof(oldMsg->mesg_data));
-
-    //printf("sending message");
-
-    //Replace the old message
-    if(SendMessage(msgQueue, newMsg) < 0)
-    {
-        printf("RemoveClientFromList: sendmessage failure.\n");
-        return -1;
-    }
-    //printf("Donerino message");
-
-    free(oldMsg);
-    free(newMsg);
-
-    return 0;
-}
-
-int SendMessage(int queue, Mesg* msg)
-{
-
-    /* This will keep trying to send messages the message queue if there are
-        too many messages in the queue. */
-    int rc = msgsnd(queue, msg, sizeof(Mesg), 0);
-
-    if (rc < 0)
-    {
-        perror("msgsnd failed:\n");
-        return -1;
-    }
-
-    return 0;
-}
-
-int AddClientToList(int msgQueue, int client)
-{
-    //need to read every message in the queue and fix it eventually.
-    Mesg *oldMsg, *newMsg;
-
-    oldMsg = (Mesg*)malloc(sizeof(Mesg));
-    newMsg = (Mesg*)malloc(sizeof(Mesg));
-
-    //printf("Reading message");
-    if(ReadMessage(msgQueue, oldMsg, 0) < 0)
-    {
-        //printf("AddClientToList: No more messages.\n");
-        return -1;
-    }
-    //printf("[num:%d]\n", oldMsg.num_sockets);
-    //printf("check message][");
-
-    //msg here, modify it to add in another client
-    if(oldMsg->num_sockets <= 0)
-    {
-        printf("AddClientToList: num_sockets lower than zero...\n");
-        return -1;
-    }
-
-    //printf("check message2\n");
-    //Do not allow too many connections
-    if(oldMsg->num_sockets >= MAXCONNECTIONS)
-    {
-        printf("AddClientToList: num{%d} > %d...\n", oldMsg->num_sockets, MAXCONNECTIONS);
-        return -1;
-    }
-    //printf("adding new client\n");
-
-    //Add in the new client and increase the number of sockets.
-    for(int i = 0; i < oldMsg->num_sockets; i++)
-    {
-        //printf("{%d}", i);
-        newMsg->sockets[i] = oldMsg->sockets[i];
-    }
-
-    newMsg->sockets[oldMsg->num_sockets] = client;
-    newMsg->num_sockets = oldMsg->num_sockets + 1;
-    newMsg->mesg_type = 1;
-    memcpy(newMsg->mesg_data, oldMsg->mesg_data, sizeof(oldMsg->mesg_data));
-
-    //printf("sending message");
-
-    //Replace the old message
-    if(SendMessage(msgQueue, newMsg) < 0)
-    {
-        printf("AddClientToList: sendmessage failure.\n");
-        return -1;
-    }
-    //printf("Donerino message");
-
-    free(oldMsg);
-    free(newMsg);
-
-    return 0;
-}
-
-int SendFinalMessage(int queue, Mesg* msg)
-{
-    msg->mesg_data[0] = '\0';
-    msg->mesg_data[1] = '\0';
-    return SendMessage(queue, msg);
-}
-
-int RemoveQueue(int queue)
-{
-    int rc;
-    rc= msgctl(queue, IPC_RMID, NULL);
-    if (rc < 0)
-    {
-        return 1;
-    }
-    return 0;
-}
-
-int OpenQueue(void)
-{
-    key_t key;
-
-    key = ftok("Info", 'a');
-    int msgQueue = msgget(key, 0644|IPC_CREAT);
-
-    if (msgQueue < 0) {
-        return -1;
-    }
-
-    return msgQueue;
-}
+#include "MessageUtilities.h"
 
 int main(void)
 {
-    int msgQueue = 0;
+    int msgQueue = 0;   //Message queue to contain the list of sockets.
+    Mesg *msg, *rcv;    //Messages for sending and receiving initially.
+    int i = 0;          //Index used for the "for" loops
 
     if((msgQueue = OpenQueue()) < 0)
     {
@@ -240,13 +12,10 @@ int main(void)
         return 1;
     }
 
-
-    Mesg *msg, *rcv;
-    int i = 0;
-
     msg = (Mesg*)malloc(sizeof(Mesg));
     rcv = (Mesg*)malloc(sizeof(Mesg));
 
+    //Assign mock sockets to the initial message.
     msg->mesg_type = 1;
     for(i = 0; i < 200; i++)
     {
@@ -254,30 +23,42 @@ int main(void)
         msg->num_sockets = i + 1;
     }
     sprintf(msg->mesg_data, "%s", "Hello.");
-    printf("[num:%d][message:%s]\n", msg->num_sockets, msg->mesg_data);
+    //printf("[num:%d][message:%s]\n", msg->num_sockets, msg->mesg_data);
 
-
-    //int ReadMessage(int queue, Mesg* msg, long msg_type)
+    //Send the first message containing all the sockets.
     if(SendMessage(msgQueue, msg) < 0)
     {
         printf("Sendmessage failure\n");
         RemoveQueue(msgQueue);
+        free(msg);
+        free(rcv);
+
         return 1;
     }
-
     printf("Sent message to queue.\n");
 
-    //test stuff in this if statement below
+
+#if 0
+    //This section will add a socket to the message.
+    //It removes the message, rearranges the message and replaces it.
     int newClient = 123;
     if(AddClientToList(msgQueue, newClient) < 0)
     {
         printf("\nUnable to add a new client.\n");
+        free(msg);
+        free(rcv);
         RemoveQueue(msgQueue);
+
         return 1;
     }
-
     printf("Added client to list.\n");
 
+#endif
+
+
+#if 0
+    //This works, this is an example of reading only a copy of the message
+    //without removing it from the message queue.
     Mesg *temp = (Mesg*)malloc(sizeof(Mesg));
 
     if(ReadMessageCopy(msgQueue, temp, 0))
@@ -289,6 +70,52 @@ int main(void)
     printf("[type:%ld][num_sockets:%d][message:%s]\n", temp->mesg_type, temp->num_sockets, temp->mesg_data);
 
     free(temp);
+
+#endif
+
+
+#if 0
+    //This removes a client's socket from the message and
+    //replace it with the new message
+    if((i = RemoveClientFromList(msgQueue, 4)) < 0) //Client remove successfully
+    {
+        if(i == -2)
+            printf("Client not found.\n");
+        else
+            printf("Critical error.\n");
+
+        free(msg);
+        free(rcv);
+        RemoveQueue(msgQueue);
+        return 1;
+    }
+    printf("Removed client from list.\n");
+
+#endif
+
+
+#if 0
+    //This fails to remove a client's socket from the message and
+    //replace it with the new message
+    if((i = RemoveClientFromList(msgQueue, 3)) < 0) //Intended failure here.
+    {
+        if(i == -2)
+        {
+            printf("Client not found.\n");
+        }
+        else
+        {
+            printf("Critical error.\n");
+            free(msg);
+            free(rcv);
+            RemoveQueue(msgQueue);
+            return 1;
+        }
+
+    }
+    printf("Test success client from list.\n");
+
+#endif
 
 
     if(ReadMessage(msgQueue, rcv, 0) < 0)
