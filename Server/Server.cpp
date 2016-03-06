0b30118ffeaa13167eaeb8f318ddc5df86553697
root <Tmanthegamer@gmail.com>
2016-03-06 15:49:07 -0800

    Pulling from repo.

--- Server/Server.cpp
+++ Server/Server.cpp
@@ -4,7 +4,7 @@
 
 #include "Server.h"
 /*---------------------------------------------------------------------------------------
---	SOURCE FILE:		mux_svr.c -   A simple multiplexed echo server using TCP
+--	SOURCE FILE:		mux_svr.c -	 A simple multiplexed echo server using TCP
 --
 --	PROGRAM:		mux.exe
 --
@@ -39,7 +39,7 @@
 #include <unistd.h>
 
 #define SERVER_TCP_PORT 7000	// Default port
-#define BUFLEN	255		        // Buffer length
+#define BUFLEN	255						// Buffer length
 #define TRUE	1
 #define LISTENQ	5
 #define MAXLINE 4096
@@ -49,156 +49,167 @@ static void SystemFatal(const char* );
 
 int main (int argc, char **argv)
 {
-    int i, maxi, nready, arg;
-    size_t bytes_to_read, total_bytes_read;
-    int listen_sd, new_sd, sockfd, port, maxfd, client[FD_SETSIZE];
-    socklen_t client_len;
-    struct sockaddr_in server, client_addr;
-    char *bp, buf[BUFLEN];
-    ssize_t n;
-    fd_set rset, allset;
+		int i, maxi, nready, arg;
+		size_t bytes_to_read = 0, total_bytes_read = 0;
+		int listen_sd, new_sd, sockfd, port, maxfd, client[FD_SETSIZE];
+		socklen_t client_len;
+		struct sockaddr_in server, client_addr;
+		char *bp, buf[BUFLEN];
+		ssize_t n;
+		fd_set rset, allset;
 
-    switch(argc)
-    {
-        case 1:
-            port = SERVER_TCP_PORT;	// Use the default port
-            break;
-        case 2:
-            port = atoi(argv[1]);	// Get user specified port
-            break;
-        default:
-            fprintf(stderr, "Usage: %s [port]\n", argv[0]);
-            exit(1);
-    }
+		switch(argc)
+		{
+				case 1:
+						port = SERVER_TCP_PORT;	// Use the default port
+						break;
+				case 2:
+						port = atoi(argv[1]);	// Get user specified port
+						break;
+				default:
+						fprintf(stderr, "Usage: %s [port]\n", argv[0]);
+						exit(1);
+		}
 
-    // Create a stream socket
-    if ((listen_sd = socket(AF_INET, SOCK_STREAM, 0)) == -1)
-        SystemFatal("Cannot Create Socket!");
+		// Create a stream socket
+		if ((listen_sd = socket(AF_INET, SOCK_STREAM, 0)) == -1)
+				SystemFatal("Cannot Create Socket!");
 
-    // set SO_REUSEADDR so port can be resused imemediately after exit, i.e., after CTRL-c
-    arg = 1;
-    if (setsockopt (listen_sd, SOL_SOCKET, SO_REUSEADDR, &arg, sizeof(arg)) == -1)
-        SystemFatal("setsockopt");
+		// set SO_REUSEADDR so port can be resused imemediately after exit, i.e., after CTRL-c
+		arg = 1;
+		if (setsockopt (listen_sd, SOL_SOCKET, SO_REUSEADDR, &arg, sizeof(arg)) == -1)
+				SystemFatal("setsockopt");
 
-    // Bind an address to the socket
-    bzero((char *)&server, sizeof(struct sockaddr_in));
-    server.sin_family = AF_INET;
-    server.sin_port = htons(port);
-    server.sin_addr.s_addr = htonl(INADDR_ANY); // Accept connections from any client
+		// Bind an address to the socket
+		bzero((char *)&server, sizeof(struct sockaddr_in));
+		server.sin_family = AF_INET;
+		server.sin_port = htons(port);
+		server.sin_addr.s_addr = htonl(INADDR_ANY); // Accept connections from any client
 
-    if (bind(listen_sd, (struct sockaddr *)&server, sizeof(server)) == -1)
-        SystemFatal("bind error");
+		if (bind(listen_sd, (struct sockaddr *)&server, sizeof(server)) == -1)
+				SystemFatal("bind error");
 
-    // Listen for connections
-    // queue up to LISTENQ connect requests
-    listen(listen_sd, LISTENQ);
+		// Listen for connections
+		// queue up to LISTENQ connect requests
+		listen(listen_sd, LISTENQ);
 
-    maxfd	= listen_sd;	// initialize
-    maxi	= -1;		// index into client[] array
+		maxfd	= listen_sd;	// initialize
+		maxi	= -1;		// index into client[] array
 
-    for (i = 0; i < FD_SETSIZE; i++)
-        client[i] = -1;             // -1 indicates available entry
-    FD_ZERO(&allset);
-    FD_SET(listen_sd, &allset);
-
-
-    while (TRUE)
-    {
-        rset = allset;               // structure assignment
-        nready = select(maxfd + 1, &rset, NULL, NULL, NULL);
-
-        if (FD_ISSET(listen_sd, &rset)) // new client connection
-        {
-            client_len = sizeof(client_addr);
-            if ((new_sd = accept(listen_sd, (struct sockaddr *) &client_addr, &client_len)) == -1)
-                SystemFatal("accept error");
-
-            printf(" Remote Address:  %s\n", inet_ntoa(client_addr.sin_addr));
-
-            for (i = 0; i < FD_SETSIZE; i++)
-                if (client[i] < 0)
-                {
-                    client[i] = new_sd;	// save descriptor
-                    break;
-                }
-            if (i == FD_SETSIZE)
-            {
-                printf ("Too many clients\n");
-                exit(1);
-            }
-
-            FD_SET (new_sd, &allset);     // add new descriptor to set
-            if (new_sd > maxfd)
-                maxfd = new_sd;	// for select
-
-            if (i > maxi)
-                maxi = i;	// new max index in client[] array
-
-            if (--nready <= 0)
-                continue;	// no more readable descriptors
-        }
-
-        for (i = 0; i <= maxi; i++)	// check all clients for data
-        {
-            if ((sockfd = client[i]) < 0)
-                continue;
-
-            if (FD_ISSET(sockfd, &rset))
-            {
-                bp = buf;
-                bytes_to_read = BUFLEN;
-                while ((n = read(sockfd, bp, bytes_to_read)) > 0)
-                {
-                    bp += n;
-                    bytes_to_read -= n;
-                    total_bytes_read += n;
-                }
-
-                if(n == 0 && total_bytes_read == 0) //The client has disconnected.
-                {
-                    printf(" Remote Address:  %s closed connection\n", inet_ntoa(client_addr.sin_addr));
-                    close(sockfd);
-                    FD_CLR(sockfd, &allset);
-                    client[i] = -1;
-                }
-                else
-                {
-                    WriteToAllClients(buf, total_bytes_read, client, maxi);
-                    total_bytes_read = 0;
-                }
-                //write(sockfd, buf, BUFLEN);   // echo to client
+		for (i = 0; i < FD_SETSIZE; i++)
+				client[i] = -1;						 // -1 indicates available entry
+		FD_ZERO(&allset);
+		FD_SET(listen_sd, &allset);
 
 
-                if (--nready <= 0)
-                    break;        // no more readable descriptors
-            }
-        }
-    }
-    return(0);
+		while (TRUE)
+		{
+				rset = allset;							 // structure assignment
+				nready = select(maxfd + 1, &rset, NULL, NULL, NULL);
+
+				if (FD_ISSET(listen_sd, &rset)) // new client connection
+				{
+						client_len = sizeof(client_addr);
+						if ((new_sd = accept(listen_sd, (struct sockaddr *) &client_addr, &client_len)) == -1)
+								SystemFatal("accept error");
+
+						printf(" Remote Address:	%s\n", inet_ntoa(client_addr.sin_addr));
+
+						for (i = 0; i < FD_SETSIZE; i++)
+								if (client[i] < 0)
+								{
+										client[i] = new_sd;	// save descriptor
+										break;
+								}
+						if (i == FD_SETSIZE)
+						{
+								printf ("Too many clients\n");
+								exit(1);
+						}
+
+						FD_SET (new_sd, &allset);		 // add new descriptor to set
+						if (new_sd > maxfd)
+								maxfd = new_sd;	// for select
+
+						if (i > maxi)
+								maxi = i;	// new max index in client[] array
+
+						if (--nready <= 0)
+								continue;	// no more readable descriptors
+				}
+
+				for (i = 0; i <= maxi; i++)	// check all clients for data
+				{
+						if ((sockfd = client[i]) < 0)
+								continue;
+
+						if (FD_ISSET(sockfd, &rset))
+						{
+							bp = buf;
+							bytes_to_read = BUFLEN;
+							while ((n = read(sockfd, bp, bytes_to_read)) > 0)
+							{
+								bp += n;
+								bytes_to_read -= n;
+								total_bytes_read += n;
+							}
+
+							printf("[total:%lu]\n", total_bytes_read);
+
+							if(total_bytes_read == 0) //The client has disconnected.
+							{
+								printf(" Remote Address:	%s closed connection\n", inet_ntoa(client_addr.sin_addr));
+								close(sockfd);
+								FD_CLR(sockfd, &allset);
+								client[i] = -1;
+								--maxi;
+							}
+							else if(n == 0) //Finished reading
+							{
+								WriteToAllClients(buf, total_bytes_read, client, maxi);
+								total_bytes_read = 0;
+							}
+
+							if (--nready <= 0)
+							{
+								printf("Left\n");
+								break; // no more readable descriptors
+							}
+
+						}
+				}
+		}
+		printf("hit");
+		return(0);
 }
 
 void WriteToAllClients(char* data, size_t datasize, int* clients, int maxi)
 {
-    int sockfd = 0;
+		int sockfd = 0;
 
-    if(maxi >= FD_SETSIZE)
-    {
-        printf ("Too many clients in WriteToAllClients\n");
-        exit(1);
-    }
+		printf("clients:%d\n", maxi);
+		if(maxi >= FD_SETSIZE)
+		{
+				printf ("Too many clients in WriteToAllClients\n");
+				exit(1);
+		}
 
-    for (int i = 0; i <= maxi; i++)
-    {
-        if ((sockfd = clients[i]) < 0)
-            continue;
+		for (int i = 0; i <= maxi; i++)
+		{
+				printf("[socket:%d]\n", clients[i]);
+				if ((sockfd = clients[i]) < 0)
+						continue;
 
-        //Ignore any errors when failing to write to a client.
-        write(sockfd, data, datasize);   // echo to client
-    }
+				printf("Sent:[%s] Size:[%lu] Client:[%d]\n", data, datasize, clients[i]);
+				//Ignore any errors when failing to write to a client.
+				send(sockfd, data, datasize, 0);	 // echo to client
+
+		}
 
 }
 
 static void SystemFatal(const char* message) {
-    perror (message);
-    exit (EXIT_FAILURE);
+		perror (message);
+		printf("hello");
+		exit (EXIT_FAILURE);
 }
